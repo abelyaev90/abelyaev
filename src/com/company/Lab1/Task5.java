0632ff7dd9d7deece31b8c418706da3b7025d045
mikuznetsova <kuznetsowa.margo@yandex.ru>
2017-08-10 20:29:11 +0600

    Сделаны задачи 1,2,3,4,5.

---
+++ src/com/company/Lab1/Task5.java
@@ -0,0 +1,14 @@
+package com.company.Lab1;
+
+import java.util.Scanner;
+
+public class Task5 {public static void main(String[] args) {
+    String a;
+    String b;
+    Scanner s = new Scanner(System.in);
+    a = s.nextLine();
+    b = s.nextLine();
+    System.out.println(b +  " " + a);
+
+}
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("It work");
+        System.out.println("It \"work");
+        System.out.println("+---+\n" +
+                "|   |\n" +
+                "|   |\n" +
+                "+---+");
+
+        // write your code here
+    }
+}
