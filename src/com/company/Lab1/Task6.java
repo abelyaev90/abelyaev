3f2063a04221a406289f01a010ec57d6f82bf757
abelyaev90 <tf37a31612@gmail.com>
2017-08-11 06:23:14 +0600

    Проверка сохранения в GIT

---
+++ src/com/company/Lab1/Task6.java
@@ -0,0 +1,16 @@
+package com.company.Lab1;
+
+import java.util.Scanner;
+
+public class Task6 {public static void main(String[] args) {
+    String a;
+    String b;
+    String c;
+    Scanner s = new Scanner(System.in);
+    a = s.nextLine();
+    b = s.nextLine();
+    c = s.nextLine();
+    System.out.println(b +  " " + a + " " + c);
+
+}
+}
