ecfd9421e05d781e7c6c3899172e2b8da4c5f7cd
abelyaev90 <tf37a31612@gmail.com>
2017-08-24 22:40:33 +0600

    Добавлен комментарий к решению задачи №22 из Лаб. №2

--- src/com/company/Lab2/Task22.java
+++ src/com/company/Lab2/Task22.java
@@ -10,7 +10,9 @@ public class Task22 {
         mass = s.nextDouble();
         alnGrad = s.nextDouble();
         if (mass <= 0 ) {
-            System.out.println("Масса должна быть положительной");
+            System.out.println("Масса должна быть положительной");/* для 4 и 5 теста: видимо, в случае,
+            ессли угол более 90 градусов - сила трения равна 0,
+             т.е. для случая alnGrad > 0 нужно обнулять результа*/
         } else{
             alnRad =(alnGrad*Math.PI)/180;
             result = mass*g*Math.cos(alnRad);
