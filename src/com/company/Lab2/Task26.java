b4be2d14529a43d6578956a695c7674a10405273
abelyaev90 <tf37a31612@gmail.com>
2017-08-25 19:15:09 +0700

    Исправлены задачи 22, 26, 30 из Лаб. №2

--- src/com/company/Lab2/Task26.java
+++ src/com/company/Lab2/Task26.java
@@ -17,7 +17,7 @@ public class Task26 {
                 System.out.println("Значение d должно быть не равно нулю");
             }
         }else{
-            r = (a*d + b*c)/a*d;
+            r = (a*d + b*c)/(a*d);
             System.out.printf("%8.4f", r);
         }
     }
