4a5102a0768a15064d608ca6ac351a0205173f67
abelyaev90 <tf37a31612@gmail.com>
2017-08-29 20:22:37 +0700

    Исправлена задача №24 из. лаб №2 (применен return)

--- src/com/company/Lab2/Task24.java
+++ src/com/company/Lab2/Task24.java
@@ -10,20 +10,20 @@ public class Task24 {
         mass1 = s.nextDouble();
         mass2 = s.nextDouble();
         distance = s.nextDouble();
-        if (mass1<=0 | mass2<=0 | distance<=0){
-            if (mass1<=0) {
-                System.out.println("Масса должна быть неотрицательной");
-            }else{
-                if (mass2<=0) {
-                    System.out.println("Масса должна быть неотрицательной");
-                } else{
-                        System.out.println("Дистанция должна быть неотрицательной");
-                    }
-                }
-            }else{
-            force = g*((mass1*mass2)/(distance*distance));
-            System.out.printf("%8.4f",force);
-
+        if (mass1 <= 0) {
+            System.out.println("Масса должна быть неотрицательной");
+            return;
         }
+        if (mass2 < 0) {
+            System.out.println("Масса должна быть неотрицательной");
+            return;
+        }
+        if (distance < 0) {
+            System.out.println("Дистанция должна быть неотрицательной");
+            return;
+        }
+        force = g * ((mass1 * mass2) / (distance * distance));
+        System.out.printf("%8.4f", force);
+
     }
 }
