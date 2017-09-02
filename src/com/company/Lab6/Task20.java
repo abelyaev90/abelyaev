cdc55cd2176bdae89f2bea9ce837832c35d52afb
abelyaev90 <tf37a31612@gmail.com>
2017-09-02 13:56:52 +0600

    начата задача №20 из. Лаб №6.

---
+++ src/com/company/Lab6/Task20.java
@@ -0,0 +1,19 @@
+package com.company.Lab6;
+
+import java.util.Scanner;
+
+public class Task20 {
+    public static void main(String[] args) {
+        Long a,b, cube;
+        Scanner s = new Scanner(System.in);
+        a = s.nextLong();
+        b = s.nextLong();
+        cube = 1L;
+        if (a<b){
+
+        }
+
+
+
+    }
+}
