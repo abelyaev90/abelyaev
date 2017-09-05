25a872c11378379e23b09f19dc5600125413f60b
abelyaev90 <tf37a31612@gmail.com>
2017-09-05 22:50:58 +0600

    начата задача 37 из Лаб №6.

---
+++ src/com/company/Lab6/Task37.java
@@ -0,0 +1,30 @@
+package com.company.Lab6;
+
+import java.util.Scanner;
+
+public class Task37 {
+    public static void main(String[] args) {
+        Integer w, h, r, q, p;
+        Scanner s = new Scanner(System.in);
+        w = s.nextInt();
+        h = s.nextInt();
+        q = 1;
+        p = 1;
+        if (w < 0) {
+            System.out.println("Значение W должно быть неотрицательно");
+            return;
+        }
+        if (h < 0) {
+            System.out.println("Значение H должно быть неотрицательно");
+            return;
+        }
+        while (p < w) {
+            for (r = q; r==1; r--) {
+                System.out.print("+");
+            }
+            q++;
+            System.out.println();
+            p++;
+        }
+    }
+}
