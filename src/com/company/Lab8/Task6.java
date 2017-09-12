a18476a9f5ceff2f8183f9ceaad52d225ff0e892
abelyaev90 <tf37a31612@gmail.com>
2017-09-12 06:01:58 +0600

    решена задача № 6 из Лаб №8.

--- src/com/company/Lab8/Task6.java
+++ src/com/company/Lab8/Task6.java
@@ -4,9 +4,36 @@ import java.util.Scanner;
 
 public class Task6 {
     public static void main(String[] args) {
-        Integer b,e;
         Scanner s = new Scanner(System.in);
+        Integer b, e, p, q;
+        String str = s.nextLine();
+        String[] arr = str.split(" ");
+        p = 0;
+        q = 0;
         b = s.nextInt();
         e = s.nextInt();
+        if (b < 0 || b > arr.length-1) {
+            System.out.println("Число B должно быть в интервале [0, размер массива)");
+            return;
+        }
+        if (e < 0 || e > arr.length-1) {
+            System.out.println("Число E должно быть в интервале [0, размер массива)");
+            return;
+        }
+        if (b <= e) {
+            while (b <= e) {
+                int n = Integer.parseInt(arr[b]);
+                System.out.print(n + " ");
+                b++;}
+            }else{
+                while (e < b) {
+                    int n = Integer.parseInt(arr[e]);
+                    System.out.print(n + " ");
+                    e++;
+                }
+
+            }
+
+        }
+
     }
-}
