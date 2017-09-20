340c94d7116d4ee5713148aa16f9e7c3ac64f344
abelyaev90 <tf37a31612@gmail.com>
2017-09-20 07:49:00 +0600

    Начата задача № 9 из Лаб №7.

---
+++ src/com/company/Lab7/Task9.java
@@ -0,0 +1,15 @@
+package com.company.Lab7;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str;
+        str = s.nextLine();
+        char a = str.charAt(0);
+       // if (a==s){
+
+        }
+    }
+}
